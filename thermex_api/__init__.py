9a3027eb47734eb18db4979c1e11fbd41da55091
hsk-dk <henrik@skjaerbaek.net>
2025-03-30 14:44:18 +0200

    Update __init__.py

--- thermex_api/__init__.py
+++ thermex_api/__init__.py
@@ -4,13 +4,20 @@ import logging
 from .const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
 
 from .api import ThermexAPI, ThermexConnectionError, ThermexAuthError
 from .const import __version__
-
+_LOGGER = logging.getLogger(__name__)
 _LOGGER.info("Setting up Thermex integration v%s", __version__)
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Thermex integration."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Thermex integration from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
     host = entry.data.get("host")
     password = entry.data.get("password")
 
