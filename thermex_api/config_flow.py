ba70c10d5d22eaeb88f6d9366fc0de777ae78f92
hsk-dk <henrik@skjaerbaek.net>
2025-05-28 10:17:09 +0200

    Update config_flow.py

--- thermex_api/config_flow.py
+++ thermex_api/config_flow.py
@@ -5,7 +5,7 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import DOMAIN
-from .api import ThermexAPI, ThermexAuthError
+from .api import ThermexAPI, ThermexAuthError, ThermexConnectionError  # Make sure ThermexConnectionError is imported
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -38,10 +38,20 @@ class ThermexConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 await api.connect(self.hass)  # Connect and authenticate
                 await api.authenticate()
                 await self.async_set_unique_id(f"thermex_{self.host}")
-                self._abort_if_unique_id_configured()
+                # Fix: return after abort to avoid continuing the flow
+                abort_result = self._abort_if_unique_id_configured()
+                if abort_result is not None:
+                    return abort_result
                 return self.async_create_entry(title=f"Thermex {self.host}", data=user_input)
             except ThermexAuthError:
+                _LOGGER.debug("Authentication failed for Thermex at %s", self.host)
                 errors["base"] = "auth_failed"
+            except ThermexConnectionError:
+                _LOGGER.debug("Could not connect to Thermex at %s", self.host)
+                errors["base"] = "cannot_connect"
+            except Exception as ex:
+                _LOGGER.exception("Unknown error in Thermex config flow: %s", ex)
+                errors["base"] = "unknown"
 
         return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
 
@@ -67,6 +77,17 @@ class ThermexConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     )
                     return self.async_abort(reason="reauth_successful")
             except ThermexAuthError:
+                _LOGGER.debug("Reauthentication failed for Thermex at %s", self.host)
                 errors["base"] = "auth_failed"
+            except ThermexConnectionError:
+                _LOGGER.debug("Could not connect to Thermex at %s during reauth", self.host)
+                errors["base"] = "cannot_connect"
+            except Exception as ex:
+                _LOGGER.exception("Unknown error in Thermex reauth flow: %s", ex)
+                errors["base"] = "unknown"
 
-        return self.async_show_form(step_id="reauth_confirm", data_schema=vol.Schema({vol.Required("password"): str}), errors=errors)
+        return self.async_show_form(
+            step_id="reauth_confirm",
+            data_schema=vol.Schema({vol.Required("password"): str}),
+            errors=errors,
+        )
